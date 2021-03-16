620bd337a5c1976a6105080d3932db7f91bb6ea6
Dave Cottlehuber <dch@skunkwerks.at>
2021-03-16 13:27:24 +0000

    separate internal logic and error handling from user interface

--- wetter.go
+++ wetter.go
@@ -9,22 +9,12 @@ import (
 
 const owmApiBaseURI = "https://api.openweathermap.org"
 
-type OWMResponse struct {
-	Weather []struct {
-		Main string
-	}
-	Main struct {
-		Kelvin float64 `json:"temp"`
-	}
-}
-
-
-type Conditions struct{
-	Summary string
+type Conditions struct {
+	Summary               string
 	TemperatureCentigrade float64
 }
 
-type Client struct{
+type Client struct {
 	APIKey string
 }
 
@@ -34,35 +24,72 @@ func NewClient(APIKey string) *Client {
 	}
 }
 
-func (c *Client) Weather(location string) (Conditions, error) {
+type owmResponse struct {
+	Weather []struct {
+		Main string
+	}
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	}
+}
+
+func Weather(owmApiToken string, owmLocation string) (Conditions, error) {
+	c := NewClient(owmApiToken)
+	
+	conditions, err := c.getOwmWeather(owmLocation)
+	if err != nil {
+		return Conditions{}, err
+	}
+	return conditions, nil
+}
+
+func (c *Client) getOwmWeather(owmLocation string) (Conditions, error) {
 	owmApiURI := fmt.Sprintf("%s/data/2.5/weather?appid=%s&q=%s",
 		owmApiBaseURI,
 		c.APIKey,
-		url.QueryEscape(location))
+		url.QueryEscape(owmLocation))
+
 	resp, err := http.Get(owmApiURI)
 	if err != nil {
-		return Conditions{}, fmt.Errorf("OpenWeatherMap API call returned an error: %w\n", err)
+		return Conditions{}, fmt.Errorf("error: OpenWeatherMap API call returned an error: %v\n", err)
 	}
 	defer resp.Body.Close()
+
+	// TLS:(*tls.ConnectionState)(0xc00051d080)}
+	// &http.Response{Status:"200 OK", StatusCode:200, Proto:"HTTP/1.1",
+	// ProtoMajor:1, ProtoMinor:1,
+	// Header:http.Header{"Access-Control-Allow-Credentials":[]string{"true"},
+	// "Access-Control-Allow-Methods":[]string{"GET, POST"},
+	// "Access-Control-Allow-Origin":[]string{"*"},
+	// "Connection":[]string{"keep-alive"}, "Content-Length":[]string{"481"},
+	// "Content-Type":[]string{"application/json; charset=utf-8"},
+	// "Date":[]string{"Tue, 02 Feb 2021 12:45:02 GMT"},
+	// "Server":[]string{"openresty"},
+	// "X-Cache-Key":[]string{"/data/2.5/weather?q=wellington"}},
+	// Body:(*http.bodyEOFSignal)(0xc000528080), ContentLength:481,
+	// TransferEncoding:[]string(nil), Close:false, Uncompressed:false,
+	// Trailer:http.Header(nil), Request:(*http.Request)(0xc000144000),
+
 	if resp.StatusCode != http.StatusOK {
-		return Conditions{}, fmt.Errorf("OpenWeatherMap API response status was unexpected: %s\n", resp.Status)
+		return Conditions{}, fmt.Errorf("error: OpenWeatherMap API response status was unexpected: %s\n", resp.Status)
 	}
 
 	// got 200, so query was valid, and data was available, this is
 	// probably parseable JSON now
 	// John's trick - read entire body into [] via ioutil.ReadAll
 	// look up io.MultiWriter for more details
-	var owmResponse OWMResponse
-	err = json.NewDecoder(resp.Body).Decode(&owmResponse)
+	var response owmResponse
+	err = json.NewDecoder(resp.Body).Decode(&response)
+
 	if err != nil {
 		return Conditions{}, fmt.Errorf("OpenWeatherMap API response was unparseable: %v\n", err)
 	}
-	if len(owmResponse.Weather) < 1 {
-		return Conditions{}, fmt.Errorf("weather array was empty: %+v", owmResponse)
+		if len(response.Weather) < 1 {
+		return Conditions{}, fmt.Errorf("weather array was empty: %+v", response)
 	}
+
 	return Conditions{
-		Summary: owmResponse.Weather[0].Main,
-		TemperatureCentigrade: owmResponse.Main.Kelvin - 273.15,
+		Summary:               response.Weather[0].Main,
+		TemperatureCentigrade: response.Main.Kelvin - 273.15,
 	}, nil
 }
-
